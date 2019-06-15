37dd18d4660d944359cf1d0fa36c55cdf175c8b1
aashishbiradar <aashish.biradar@gmail.com>
2019-06-15 18:50:03 +0530

    send tasks from lamp server in php

---
+++ sendtaskspage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script defer src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous">
+    </script>
+</head>
+<body>
+    <div>
+    <h1>Send Tasks</h1>
+    </div>
+    <div>
+        <button id="send">Send</button>
+    </div>
+</body>
+<script>
+    $('#send').off().on('click',function(){
+        $.ajax({
+            method: "POST",
+            url: "/sendtasks.php",
+            data: {
+                args:{},
+                noOfTasks: 1
+            },
+            complete : function(res) {
+                console.log(res);
+            }
+        })
+    });
+</script>
+</html>
