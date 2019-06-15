4ee3a5a1ca28fb2f0e97841422d45e11675e2044
aashishbiradar <aashish.biradar@gmail.com>
2019-06-16 00:56:14 +0530

    send msg

--- sendtasks.php
+++ sendtasks.php
@@ -15,6 +15,16 @@ try
         $CONFIG['user'],        #user - username to connect to server
         $CONFIG['password']         #password
         );
+    
+    $channel->queue_declare('hello', false, false, false, false);
+
+    $msg = new AMQPMessage('Hello World!');
+    $channel->basic_publish($msg, '', 'hello');
+    
+    echo " [x] Sent 'Hello World!'\n";
+
+    $channel->close();
+    $connection->close();
 }
 catch(exception $e)
 {
