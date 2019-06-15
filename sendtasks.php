01a7439e89d4fea9116b944d2161347b267333e6
aashishbiradar <aashish.biradar@gmail.com>
2019-06-15 19:21:56 +0530

    added connection

--- sendtasks.php
+++ sendtasks.php
@@ -1,3 +1,17 @@
 <?php
-print_r($_POST);
+require_once('../local/config.php');
+try
+{
+    $connection = new AMQPConnection(
+        $CONFIG['host'],    #host - host name where the RabbitMQ server is runing
+        5672,           #port - port number of the service, 5672 is the default
+        'guest',        #user - username to connect to server
+        'guest'         #password
+        );
+}
+catch(exception $e)
+{
+    print_r($e);
+}
+echo "Success!"
 ?>
