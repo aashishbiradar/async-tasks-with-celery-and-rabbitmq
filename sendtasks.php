0e7de2d3cbef7b976a4943e4f6edacf0ff6742c8
aashishbiradar <aashish.biradar@gmail.com>
2019-06-15 19:49:20 +0530

    included dependencies

--- sendtasks.php
+++ sendtasks.php
@@ -1,6 +1,11 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
 require_once('../local/config.php');
 try
 {
