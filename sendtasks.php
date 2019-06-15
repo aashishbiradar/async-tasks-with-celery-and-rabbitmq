7b5bcb0c77348735cf912b2c7478958c7e3af30e
aashishbiradar <aashish.biradar@gmail.com>
2019-06-16 02:31:00 +0530

    post celery task

--- sendtasks.php
+++ sendtasks.php
@@ -2,14 +2,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-require_once('../vendor/autoload.php');
+/*require_once('../vendor/autoload.php');
 use PhpAmqpLib\Connection\AMQPConnection;
-use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\Message\AMQPMessage;*/
 
 require_once('../local/config.php');
 try
 {
-    $connection = new AMQPConnection(
+    /*$connection = new AMQPConnection(
         $CONFIG['host'],    #host - host name where the RabbitMQ server is runing
         $CONFIG['port'],           #port - port number of the service, 5672 is the default
         $CONFIG['user'],        #user - username to connect to server
@@ -25,7 +25,10 @@ try
     echo " [x] Sent 'Hello World!'\n";
 
     $channel->close();
-    $connection->close();
+    $connection->close();*/
+    require_once '../celery-php/vendor/autoload.php';
+    $c = new \Celery\Celery($CONFIG['host'], $CONFIG['user'], $CONFIG['password'], $CONFIG['vhost']);
+    $result = $c->PostTask('tasks.reverse', 'fromphp');
 }
 catch(exception $e)
 {
